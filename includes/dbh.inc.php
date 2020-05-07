517b8ff4e4ffb7a92d184946bb176864511df972
Ian <kamanuian@gmail.com>
2020-05-07 20:37:04 +0300

    Project Code

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 $server_name = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_name = "new_users";
+$db_username = "id13523738_root";
+$db_password = "Q0<r>2YwojuFll|6";
+$db_name = "id13523738_africa_explored";
 
 $conn = mysqli_connect($server_name, $db_username, $db_password, $db_name);
 
